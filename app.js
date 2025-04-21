95cac81632b6c71266ad33f98491b9d577fc93ec
MuhammadZareef <muhammadzareef709@gmail.com>
2025-04-21 22:44:45 +0500

    Initialize

--- app.js
+++ app.js
@@ -1,57 +1,64 @@
 
 console.log("Hello World!");
 
-let featureImage = document.querySelector('#feature-image');
-let sideFeaturedImg1 = document.querySelector('#side-featured-img1');
-let sideFeaturedImg2 = document.querySelector('#side-featured-img2');
-let sideFeaturedImg3 = document.querySelector('#side-featured-img3');
-
-let newsImg1 = document.querySelector('#news-img1');
-let newsImg2 = document.querySelector('#news-img2');
-let newsImg3 = document.querySelector('#news-img3');
-let newsImg4 = document.querySelector('#news-img4');
-let newsImg5 = document.querySelector('#news-img5');
-let newsImg6 = document.querySelector('#news-img6');
-
-function uploadeImages(articles) {
-    featureImage.src = `${articles[0].image}`;
-    sideFeaturedImg1.src = `${articles[1].image}`;
-    sideFeaturedImg2.src = `${articles[2].image}`;
-    sideFeaturedImg3.src = `${articles[3].image}`;
-
-    newsImg1.src = `${articles[4].image}`;
-    newsImg2.src = `${articles[5].image}`;
-    newsImg3.src = `${articles[6].image}`;
-    newsImg4.src = `${articles[7].image}`;
-    newsImg5.src = `${articles[8].image}`;
-    newsImg6.src = `${articles[9].image}`;
+function generateCard(articles, times, category) {
+    let featuredNews = document.querySelector('#featuredNews');
+    featuredNews.innerHTML = "";
+    featuredNews.innerHTML += `
+        <article class="main-featured">
+            <img src="${articles[0].image}" alt="Featured News">
+            <div class="main-featured-content">
+                <span class="news-category">Politics</span>
+                <h2 class="news-title">${articles[0].title}</h2>
+                <div class="news-meta">
+                    <span>By ${articles[0].source.name}</span>
+                    <span>2 hours ago</span>
+                </div>
+            </div>
+        </article>
+        <div class="side-featured" id="sideFeatured">
+        </div>
+    `;
+    let sideFeatured = document.getElementById('sideFeatured');
+    sideFeatured.innerHTML = "";
+    for(let i = 1; i <= 3; i++) {
+        sideFeatured.innerHTML += `
+        <article class="side-featured-article">
+            <div class="side-featured-img">
+                <img src="${articles[i].image}" alt="Tech News">
+            </div>
+            <div class="side-featured-content">
+                <span class="news-category">${category}</span>
+                <h3 class="news-title">${articles[i].title}</h3>
+            </div>
+        </article>
+        `;
+    }
+    let newsGrid = document.querySelector('#news-grid');
+    newsGrid.innerHTML = "";
+    for(let i = 4; i <= 9; i++) {
+    newsGrid.innerHTML += `
+    <article class="news-card" onclick="window.open('${articles[i].url}', '_blank')">
+        <div class="news-img">
+            <img src="${articles[i].image}" alt="News-Image">
+        </div>
+        <div class="news-content">
+            <span class="news-category">${category}</span>
+            <h3 class="news-title">${articles[i].title}</h3>
+            <p class="news-excerpt">${articles[i].description}</p>
+            <div class="news-meta">
+                <span>By ${articles[i].source.name}</span>
+                <span>5 hours ago</span>
+            </div>
+        </div>
+    </article>
+    `;
+    }
 }
 
-let newsTitle1 = document.querySelector('#news-title1');
-let newsTitle2 = document.querySelector('#news-title2');
-let newsTitle3 = document.querySelector('#news-title3');
-let newsTitle4 = document.querySelector('#news-title4');
-let newsTitle5 = document.querySelector('#news-title5');
-let newsTitle6 = document.querySelector('#news-title6');
-let newsTitle7 = document.querySelector('#news-title7');
-let newsTitle8 = document.querySelector('#news-title8');
-let newsTitle9 = document.querySelector('#news-title9');
-let newsTitle10 = document.querySelector('#news-title10');
-
-function uploadTitles(articles) {
-    newsTitle1.textContent = `${articles[0].title}`;
-    newsTitle2.textContent = `${articles[1].title}`;
-    newsTitle3.textContent = `${articles[2].title}`;
-    newsTitle4.textContent = `${articles[3].title}`;
-    newsTitle5.textContent = `${articles[4].title}`;
-    newsTitle6.textContent = `${articles[5].title}`;
-    newsTitle7.textContent = `${articles[6].title}`;
-    newsTitle8.textContent = `${articles[7].title}`;
-    newsTitle9.textContent = `${articles[8].title}`;
-    newsTitle10.textContent = `${articles[9].title}`;
-}
-
-const apiKey = '57e0c4edb06ebe07bc2f0bc5038833fe';
+// const apiKey = '57e0c4edb06ebe07bc2f0bc5038833fe'; // 1st
+// const apiKey = '990d1d3ee0ae548225337c78af36c79d'; // 2nd
+const apiKey = '2bd652ee5055f3315739af5f3761d77d'; // 3rd
 // const apiKey = '9eca228010cb4289a16860fc8d9877db';
 // const apiKey = '182b1ca07013422f8261831bfc5a25e8';
 
@@ -68,14 +75,34 @@ fetch(proxyURL)
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        uploadeImages(data.articles);
-        uploadTitles(data.articles);
+        generateCard(data.articles, 9, 'Sports');
         // data.articles.forEach(article => {});
     })
     .catch(err => console.error('Error:', err))
-
 
     //   You must activate your account to use the API. If you did not receive the email to activate your account, you can request a new one here: https://gnews.io/dashboard"
     //   length
 
     // Platzi Fake Store API
+
+    changeCategory('Home');
+
+function changeCategory(curr) {
+    if (curr == "Home" || curr.textContent == "Home") {
+        let heroSection = document.querySelector('#hero');
+        heroSection.innerHTML = `
+        <section class="hero" id="hero">
+        <div class="container">
+            <div class="hero-content">
+                <h1>Stay Informed with the Latest News</h1>
+                <p>Get real-time updates on global events, business, technology, and more. Your trusted source for accurate and timely information.</p>
+                <a href="#" class="btn">Subscribe Now</a>
+            </div>
+        </div>
+    </section>
+    `;
+    }
+    let category = curr.textContent;
+    // generateCard(data.category, 1, category);
+}
+
