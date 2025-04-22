94b49d04b3970d71efbca1c5d9f239c6293aa8e3
MuhammadZareef <muhammadzareef709@gmail.com>
2025-04-22 18:26:41 +0500

    Upload

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 
 console.log("Hello World!");
 
-function generateCard(articles, times, category) {
+const mobileMenuBtn = document.getElementById('mobileMenuBtn');
+const navMenu = document.getElementById('navMenu');
+
+mobileMenuBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+});
+
+function renderFeaturedSection(articles, category) {
     let featuredNews = document.querySelector('#featuredNews');
     featuredNews.innerHTML = "";
     featuredNews.innerHTML += `
-        <article class="main-featured">
+        <article class="main-featured" onclick="window.open('${articles[0].url}', '_blank')">
             <img src="${articles[0].image}" alt="Featured News">
             <div class="main-featured-content">
-                <span class="news-category">Politics</span>
+                <span class="news-category">${category}</span>
                 <h2 class="news-title">${articles[0].title}</h2>
                 <div class="news-meta">
                     <span>By ${articles[0].source.name}</span>
@@ -23,7 +30,7 @@ function generateCard(articles, times, category) {
     sideFeatured.innerHTML = "";
     for(let i = 1; i <= 3; i++) {
         sideFeatured.innerHTML += `
-        <article class="side-featured-article">
+        <article class="side-featured-article" onclick="window.open('${articles[i].url}', '_blank')">
             <div class="side-featured-img">
                 <img src="${articles[i].image}" alt="Tech News">
             </div>
@@ -34,6 +41,9 @@ function generateCard(articles, times, category) {
         </article>
         `;
     }
+}
+
+function renderCards(articles, times, category) {
     let newsGrid = document.querySelector('#news-grid');
     newsGrid.innerHTML = "";
     for(let i = 4; i <= 9; i++) {
@@ -56,40 +66,51 @@ function generateCard(articles, times, category) {
     }
 }
 
-// const apiKey = '57e0c4edb06ebe07bc2f0bc5038833fe'; // 1st
-// const apiKey = '990d1d3ee0ae548225337c78af36c79d'; // 2nd
-const apiKey = '2bd652ee5055f3315739af5f3761d77d'; // 3rd
+
 // const apiKey = '9eca228010cb4289a16860fc8d9877db';
 // const apiKey = '182b1ca07013422f8261831bfc5a25e8';
 
-const gnewsURL = `https://gnews.io/api/v4/top-headlines?token=${apiKey}&lang=en&max=10`;
-
-const proxyURL = `https://api.allorigins.win/raw?url=${encodeURIComponent(gnewsURL)}`;
-
-let category = "sports";
-
-let url = 'https://gnews.io/api/v4/top-headlines?category=' + category + '&lang=en&country=pak&max=10&apikey=' + apiKey;
-// let url = 'https://gnews.io/api/v4/top-headlines?category=' + category + '&lang=en&country=us&max=10&apikey=' + apiKey;
-
-fetch(proxyURL)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        generateCard(data.articles, 9, 'Sports');
-        // data.articles.forEach(article => {});
-    })
-    .catch(err => console.error('Error:', err))
+// fetch(proxyURL)
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//         // renderCards(data.articles, 9, 'Sports');
+//         // data.articles.forEach(article => {});
+//     })
+//     .catch(err => console.error('Error:', err))
 
     //   You must activate your account to use the API. If you did not receive the email to activate your account, you can request a new one here: https://gnews.io/dashboard"
     //   length
 
     // Platzi Fake Store API
 
-    changeCategory('Home');
+    // changeCategory('Home');
+
+    let heroSection = document.querySelector('#hero');
+
+    const allTabs = document.querySelectorAll('#navMenu a');
+    allTabs[0].classList.add('active');
 
 function changeCategory(curr) {
+    let category = curr.textContent;
     if (curr == "Home" || curr.textContent == "Home") {
-        let heroSection = document.querySelector('#hero');
+        category = "sports";
+        heroSection.className = "";
+        renderHeroSection();
+    } else {
+        heroSection.innerHTML = "";
+        heroSection.className = "section";
+    }
+    let url = getURL(category);
+    getData(url, category);
+    
+    allTabs.forEach(tab => tab.classList.remove('active'));
+    curr.classList.add('active');
+}
+
+renderHeroSection();
+
+function renderHeroSection() {
         heroSection.innerHTML = `
         <section class="hero" id="hero">
         <div class="container">
@@ -101,8 +122,38 @@ function changeCategory(curr) {
         </div>
     </section>
     `;
-    }
-    let category = curr.textContent;
-    // generateCard(data.category, 1, category);
 }
 
+function getURL(category) {
+    // const apiKey = '57e0c4edb06ebe07bc2f0bc5038833fe'; // 1st
+    const apiKey = '990d1d3ee0ae548225337c78af36c79d'; // 2nd
+    // const apiKey = '2bd652ee5055f3315739af5f3761d77d'; // 3rd
+
+    // let category = "business";
+
+    category = category.toLowerCase();
+    console.log(category);
+
+    // let url = 'https://gnews.io/api/v4/top-headlines?category=' + category + '&lang=en&country=pak&max=10&apikey=' + apiKey;
+    let url = 'https://gnews.io/api/v4/top-headlines?category=' + category + '&lang=en&country=us&max=10&apikey=' + apiKey;
+
+    const gnewsURL = `https://gnews.io/api/v4/top-headlines?token=${apiKey}&lang=en&max=10`;
+
+    const proxyURL = `https://api.allorigins.win/raw?url=${encodeURIComponent(gnewsURL)}`;
+
+    return url;
+}
+
+// const URL = 'https://jsonplaceholder.typicode.com/todos/';
+const URL = getURL("sports");
+
+// getData(URL);
+getData(URL, "Sports");
+
+async function getData(URL, category) {
+    let response = await fetch(URL);
+    let data = await response.json();
+    console.log(data);
+    renderFeaturedSection(data.articles, category);
+    renderCards(data.articles, 5, category);
+}
